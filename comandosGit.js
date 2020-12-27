4896e546dc8c7a9c250c2514df58a310c537dc9b
IgorSouzaGit <ig_souza@outlook.com>
2020-12-27 11:52:48 -0300

    Commit com Atualizaçoes

--- comandosGit.js
+++ comandosGit.js
@@ -1,5 +1,9 @@
-git clone 
-git add .
-git commit -m"..."
-git push
-git pull
+Apos a instalação colocar configurações globais de usuario
+    git config --global user.email "email@email"
+    git config --global user.name "User@github"
+
+git clone --> clona um repositorio no github
+git add . --> adiciona arquivos no repositorio local para ser enviados ao github
+git commit -m"..." --> adiciona uma mensagem ao envio
+git push --> envia arquivos ao repositorio github
+git pull --> recupera/atualiza repositorio local 
